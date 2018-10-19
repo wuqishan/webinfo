c84701f4937617e604e3321a705c1c66af7947c1
wells <qishan.wu.1@lexisnexis.com>
2018-10-19 17:56:41 +0800

    refs

--- webinfo/settings.py
+++ webinfo/settings.py
@@ -45,7 +45,11 @@ ROBOTSTXT_OBEY = True
 #}
 DEFAULT_REQUEST_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3423.2 Safari/537.36',
-    'Accept-Language': 'en-US,en;q=0.9'
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Connection': 'keep-alive',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Cache-Control': 'max-age=0'
 }
 
 # Enable or disable spider middlewares
