b174614ee0b8585c6467fba5e963f5e10bf412aa
wells <qishan.wu.1@lexisnexis.com>
2018-10-19 17:47:20 +0800

    refs

--- webinfo/spiders/MaoYanSpider.py
+++ webinfo/spiders/MaoYanSpider.py
@@ -6,13 +6,13 @@ from webinfo.items import LexisItem
 
 class LexisSpider(scrapy.Spider):
     # 这个name是你必须给它一个唯一的名字  后面我们执行文件时的名字
-    name = "lexis"
+    name = "maoyan"
 
-    allowed_domains = ["hk.lexiscn.com"]
+    allowed_domains = ["maoyan.com"]
 
     # 这个列表中的url可以有多个，它会依次都执行，我们这里简单爬取一个
     start_urls = [
-        "http://hk.lexiscn.com/search/index?keyword=%E5%85%AC%E5%8F%B8%E6%B3%95"
+        "http://maoyan.com/films"
     ]
 
     # 因为豆瓣250有翻页操作，我们设置这个url用来翻页
